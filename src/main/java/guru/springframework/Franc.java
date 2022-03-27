3bf08cf5bc54d893d1d793396294728f66e67b16
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-27 21:11:45 +0300

    Adds the Franc currency

---
+++ src/main/java/guru/springframework/Franc.java
@@ -0,0 +1,18 @@
+package guru.springframework;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return franc.amount == amount;
+    }
+}
