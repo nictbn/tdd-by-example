d29e136d3c32b67d1b3d9c843f1129614c8b7ba8
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-29 00:39:43 +0300

    Creates sum expression, adds reduce operation for sum and money

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -2,6 +2,9 @@ package guru.springframework;
 
 public class Bank {
     Money reduce(Expression source, String toCurrency) {
-        return Money.dollar(10);
+        return source.reduce(toCurrency);
+//        if (source instanceof Money) return (Money) source;
+//        Sum sum = (Sum) source;
+//        return sum.reduce(toCurrency);
     }
 }
